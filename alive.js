ba7fd7675180a509f8d7905cdd76190483691408
djasuu <othumanmsomi@gmail.com>
2025-03-10 17:55:30 +0300

    Update alive.js

--- alive.js
+++ alive.js
@@ -43,7 +43,7 @@ const date = moment().format('DD/MM/YYYY');
  ${message}
  
  
- *ANYWAY-MD-WABOT*`
+ *ASUU-MD-WABOT*`
 
  if (lien.match(/\.(mp4|gif)$/i)) {
     try {
@@ -88,7 +88,7 @@ else {
     
 await addOrUpdateDataInAlive(texte , tlien)
 
-repondre(' Holla🥴, *ANYWAY-MD BOT* is alive just like you gee. ')
+repondre(' Holla🥴, *ASUU-MD BOT* is alive just like you gee. ')
 
 }
     });
